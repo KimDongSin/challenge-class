873909530bed4031cedf6f99c2de5cd2e8af93dc
KimDongSin <asstaff2020@gmail.com>
2024-05-20 03:35:38 +0900

    캐릭터 상하좌우 이동, 맵 밖으로 나가지 않게 처리

--- assignments/02/pikachuGame/src/components/Game/GameContainer.jsx
+++ assignments/02/pikachuGame/src/components/Game/GameContainer.jsx
@@ -1,5 +1,65 @@
+import { useEffect, useRef, useState } from "react";
+import char from "./../../assets/pikachu.png";
+
 function GameContainer() {
-  return <div>GameContainer</div>;
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const charRef = useRef(null);
+
+  const handleKeydown = (e) => {
+    // 캐릭터 위치 값
+    let topVal = charRef.current.style.top.replace("px", "");
+    let leftVal = charRef.current.style.left.replace("px", "");
+
+    // 캐릭터 기준으로 상하좌우
+    switch (e.key) {
+      case "ArrowUp":
+        if (topVal >= 50) {
+          setPosition((prev) => ({ ...prev, top: prev.top - 50 }));
+        }
+        break;
+      case "ArrowDown":
+        if (topVal < 450) {
+          setPosition((prev) => ({ ...prev, top: prev.top + 50 }));
+        }
+        break;
+      case "ArrowLeft":
+        if (leftVal >= 50) {
+          setPosition((prev) => ({ ...prev, left: prev.left - 50 }));
+        }
+        break;
+      case "ArrowRight":
+        if (leftVal < 450) {
+          setPosition((prev) => ({ ...prev, left: prev.left + 50 }));
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
+  return (
+    <main>
+      <div className="bg__container">
+        <img
+          className="game__char"
+          ref={charRef}
+          src={char}
+          alt="피카츄"
+          style={{
+            top: `${position.top}px`,
+            left: `${position.left}px`,
+          }}
+        />
+      </div>
+    </main>
+  );
 }
 
 export default GameContainer;
